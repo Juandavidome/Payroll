c39939e8edd907be02a4d92cdcab8a2163586c09
Juan David Ome <104031677+Juandavidome@users.noreply.github.com>
2022-06-22 14:14:39 -0500

    Add files via upload

---
+++ PAYROLL/Data.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PAYROLL
+{
+    internal class Data
+    {
+        public static Employees ReadEmplo(Employees employee1)
+        {
+
+            try
+            {
+                Console.WriteLine("Insert the document:");
+                employee1.DocumentId = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Insert the Name:");
+                employee1.Name = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Insert the Last Name:");
+                employee1.LastName = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Insert the salary:");
+                employee1.Salary = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.WriteLine("Insert the days worked:");
+                employee1.WorkedDays = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine("El value no is valid"+ exception);
+                Console.ReadKey();
+            }
+
+            return employee1;
+        }
+
+        public static Employees Calculate(Employees employee1)
+        {
+            
+           
+
+            employee1.TotalAccrued = employee1.Salary / 30;
+            employee1.TotalAccrued = employee1.TotalAccrued * employee1.WorkedDays;
+
+            employee1.AuxTransport = 117172 / 30;
+            employee1.AuxTransport = employee1.AuxTransport * employee1.WorkedDays;
+
+            employee1.Health = employee1.TotalAccrued * 0.4;
+            employee1.Pension = employee1.TotalAccrued * 0.4;
+
+            if (employee1.Salary <= 2000000)
+            {
+                employee1.TotalAccrued = employee1.TotalAccrued + employee1.AuxTransport;
+            }
+            else
+            {
+                employee1.TotalAccrued = employee1.TotalAccrued;
+            }
+
+            return employee1;
+        }
+
+        public static Employees PrintData(Employees employee1)
+        {
+            Console.WriteLine($"Document number:{employee1.DocumentId} ");
+            Console.WriteLine($"First Name: {employee1.Name}");
+            Console.WriteLine($"Last Name: {employee1.LastName}");
+            Console.WriteLine($"Salary: {employee1.Salary}");
+            Console.WriteLine($"Worked_Days: {employee1.WorkedDays}");
+            Console.WriteLine($"Total Accrued: {employee1.TotalAccrued}");
+            Console.WriteLine($"Health: {employee1.Health}");
+            Console.WriteLine($"Pension: {employee1.Pension}");
+            return employee1;
+        }
+
+        public static void Save(List<Employees> employee1)
+        {
+            string Name1 = "C:\\Employee.txt";
+            try
+            {
+                string Json = JsonConvert.SerializeObject(employee1, Formatting.Indented);
+                File.WriteAllText(Name1, Json);
+             
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("ERROR", exception);
+
+            }
+        }
+    }
+}
---
+++ PAYROLL/Payroll.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAYROLL
+{
+    internal class Payroll
+    {
+        public static void Main(string[] args)
+        {
+            Employees employee1 = new Employees();
+            Data data = new Data();
+
+
+            Console.WriteLine("Welcome to your payroll:");
+            int Opc1 = 1;
+
+            List<Employees> employees = new List<Employees>();
+            do
+            {
+                Console.WriteLine($"EMPLOYE: ");
+                Console.WriteLine("1.Press if you want to enter the employee");
+                Console.WriteLine("2.Press if you want to exit");
+                Opc1 = int.Parse(Console.ReadLine());
+                switch (Opc1)
+                {
+                    case 1:
+
+                        Data.ReadEmplo(employee1);
+                        Data.Calculate(employee1);
+                        Data.PrintData(employee1);
+                        employees.Add(employee1);
+                        Data.Save(employees);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("__________________________________________________");
+                        Console.WriteLine("Exit the program");
+                        break;
+                    default:
+                        Console.WriteLine("The option not is valid. ");
+                        break;
+
+                }
+                Console.ReadKey();
+
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Clear();
+
+            } while (Opc1 != 2);
+        }
+    }
+}
